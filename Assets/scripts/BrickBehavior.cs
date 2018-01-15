7641151cf71ccbb229975e4d4813f3e252617937
emk-reish <emma.karin.r@gmail.com>
2018-01-15 13:19:38 -0600

    added brick classes

---
+++ Assets/scripts/BrickBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickBehavior : MonoBehaviour {
+
+	string brickID = "";
+
+	public BrickBehavior(string id, float distance, double startAngle, double endAngle) {
+		brickID = id;
+		this.CreateBrickInGame(distance, startAngle, endAngle);
+	}
+
+	void CreateBrickInGame(float d, double a, double x) {
+		Vector2[] vertices; 
+
+
+		Mesh mesh = GetComponent<MeshFilter>().mesh;
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/scripts/BrickMeshBehavior.cs
@@ -0,0 +1,42 @@
+﻿using System; 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickMeshBehavior : MonoBehaviour {
+
+	[Range(1,10)]
+	public int maxLevel = 3;
+
+	[Range(10, 40)]
+	public int numBricks = 20;
+
+	public float baseRadius = 2.7f;
+	public float ringDepth = .5f;
+
+	private int level = 1;
+
+	private double angle;
+
+	// Use this for initialization
+	void Start () {
+		angle = 2 * Math.PI / numBricks;
+
+		if(level <= maxLevel) {
+			for(int i = 0; i < level; i++) {
+				this.MakeRing(i);
+			}
+		} else {
+			// end game
+		}
+	}
+
+	void MakeRing(int ringCount) {
+		float radius = baseRadius + ringDepth * level;
+		for(int n = 0; n < numBricks; n++) {
+			double start = angle * n;
+			string id = ringCount + "." + n;
+			BrickBehavior brick = new BrickBehavior(id, radius, start, start + angle);
+		}
+	}
+}
